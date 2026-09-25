ca46fecc56a94d6e424a217176d9759a73b191cb
Xeraaa11 <maxence.nunes@hes-so.ch>
2026-09-25 14:33:15 +0200

    création du fichier CLion + début du code

---
+++ main.cpp
@@ -0,0 +1,28 @@
+// Appel des librairies
+#include <iostream>
+using namespace std;
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+int main() {
+    //Création des variables
+    int L1 = 6;
+    int L2;
+    int SpeedL1 = 5;
+    int SpeedL2 = 2;
+    int dy = 10;
+    int dx = 3;
+
+    //1.
+    cout << "Bonjour utilisateur,\n";
+    cout << "Ce programme va servir a calculer le temps que va mettre un robot a se deplacer sur une certaine distance.\n\n";
+
+    //2.
+    cout << "Veuillez maintenant saisir la distance L1 que le robot devra parcourir.  (un entier)";
+    //Verifications
+
+
+    // 3.
+    cout << {dy - L1}^2 + {dx}^2
+
+    return 0;
+}
